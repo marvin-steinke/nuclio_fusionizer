24db104d3436fac78e9201c961689cdb57c76445
Fabian Kreßin <61588952+fabiankressin@users.noreply.github.com>
2023-12-03 23:37:37 +0100

    added more endpoint-stubs

--- server/nuclio_fusionizer_server/api_server.py
+++ server/nuclio_fusionizer_server/api_server.py
@@ -1,38 +1,52 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI, Form, UploadFile, File
 import uvicorn
+from typing import Annotated
+from pydantic import BaseModel
+
 
 class ApiServer:
     def __init__(self):
         # Create a FastAPI app instance
         self.app = FastAPI()
 
-        @app.put("/deploy/") # (re)-deploy
-        async def deploy(
-            function_name: str = Form(...), 
-            namespace: str = Form(...),
-            function_configuration: UploadFile = File(None), # Optional
-            function: UploadFile = File(None) # Optional
-        ):
-            if function_configuration:
-                # TODO what path to save tmp config file?
-                config_path = f"/path/to/save/config/{function_configuration.filename}"
-                with open(config_path, "wb") as buffer:
-                    buffer.write(function_configuration.file.read())
+        class DeployRequest(BaseModel):
+            function_name: str = Form(...)
+            namespace: str = Form(...)
+            function_configuration: UploadFile = None  # Optional
+            function: UploadFile = None  # Optional
 
-            if function:
+        @self.app.put("/deploy/")  # (re)-deploy
+        async def deploy(deploy_request: Annotated[DeployRequest, Body()]):
+            if deploy_request.function_configuration:
+                # TODO what path to save tmp config file?
+                config_path = f"/path/to/save/config/{deploy_request.function_configuration.filename}"
+                with open(config_path, "wb") as buffer:
+                    buffer.write(deploy_request.function_configuration.file.read())
+
+            if deploy_request.function:
                 # TODO what path to save tmp function file?
-                function_path = f"/path/to/save/functions/{function.filename}"
+                function_path = f"/path/to/save/functions/{deploy_request.function.filename}"
                 with open(function_path, "wb") as buffer:
-                    buffer.write(function.file.read())
+                    buffer.write(deploy_request.function.file.read())
 
             # TODO add deploy logic
 
             # TODO return invoke endpoint
             return {"status": "success"}
 
-        @app.put("/delete/{function_name}")
+        @self.app.delete("/delete/{function_name}")
         async def delete(function_name: str):
             # TODO add deletion logic
+            return {"status": "success", "function_name": function_name}
+
+        @self.app.get("/get/{function_name}")
+        async def get(function_name: str):
+            # TODO add logic to get resource information
+            return {"status": "success", "function_name": function_name}
+
+        @self.app.put("/invoke/{function_name}")
+        async def invoke(function_name: str):
+            # TODO add logic to invoke a function
             return {"status": "success", "function_name": function_name}
 
     def run(self):
--- server/nuclio_fusionizer_server/main.py
+++ server/nuclio_fusionizer_server/main.py
@@ -1,5 +1,9 @@
+from server.nuclio_fusionizer_server.api_server import ApiServer
+
+
 def main():
-    pass
+    api_server = ApiServer()
+    api_server.run()
 
 if __name__ == "__main__":
     main()
